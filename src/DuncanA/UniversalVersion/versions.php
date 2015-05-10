41d9d2f682fb2033f3b84110891fd68722a8cc24
DuncanA <mccleanduncan03@hotmail.com>
2015-05-10 07:13:14 +0100

    Create versions.php

---
+++ src/DuncanA/UniversalVersion/versions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace \DuncanA\UniversalVersion;
+
+use pocketmine\plugin\PluginBase;
+
+class Versions extends PluginBase(){
+
+   public function Versions(VerHandeler $version){
+   	//Manages the packets from 0.10.4
+   	(packet1)
+   	//Manages the packets from 0.11.0
+   	(packet2)
+   	
+}
